d37a73cf8575c03a612af9c918cbd1f26da00c6d
pugalo0123 <nicglad2005@gmail.com>
2025-12-19 21:31:53 +0700

    create index.html, modify main middleware

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def send_my_promt(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:5500'],
+    allow_origins=['http://127.0.0.1:5500'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
